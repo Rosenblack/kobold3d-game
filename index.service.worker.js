d9ceed57f7c7e8521ec4fe972f98aa1e2911df88
Bunno <gakupo.gumi@gmail.com>
2025-10-27 20:12:03 +1300

    alternating colour tiles added

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761548390|442540495';
+const CACHE_VERSION = '1761549083|357688402';
 /** @type {string} */
 const CACHE_PREFIX = 'kobold3d V2-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
