18c632822f39e1d70c81e08a55a3431f270f64f1
Bunno <gakupo.gumi@gmail.com>
2025-10-30 10:09:26 +1300

    gui press graphics

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761734724|901649239';
+const CACHE_VERSION = '1761772111|819627736';
 /** @type {string} */
 const CACHE_PREFIX = 'kobold3d V2-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
